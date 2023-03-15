ba668b95b9f48a0d367e0cde68b775a389fd4d8c
shweta.jivtode <shweta.jivtode@NAGSEZ545633L.ad.infosys.com>
2023-03-15 10:43:40 +0530

    less commit

---
+++ main/src/main/less.java
@@ -0,0 +1,10 @@
+package main;
+
+public class less {
+	public static void main(String args[]){
+		 System.out.println("Hello meri");
+		 System.out.println("Hello teri");
+		 System.out.println("Hello tery");
+
+
+}
